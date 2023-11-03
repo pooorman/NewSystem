b9c7b92b22575648fadb509b61ab2351b4d614ef
Zhang <2939597933@qq.com>
2023-11-03 11:49:03 +0800

    项目搭建初版1.0.1

---
+++ src/main/java/com/example/newsystem01/Dao/Blogger.java
@@ -0,0 +1,39 @@
+package com.example.newsystem01.Dao;
+
+public class Blogger {
+    private Long id;
+    private String name;
+    private String pass;
+    // 省去set和get
+
+
+    public Blogger(Long id, String name, String pass) {
+        this.id = id;
+        this.name = name;
+        this.pass = pass;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
---
+++ src/main/java/com/example/newsystem01/controller/ExceptionController.java
@@ -0,0 +1,25 @@
+package com.example.newsystem01.controller;
+
+import com.example.newsystem01.JsonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception")
+public class ExceptionController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+    @PostMapping("/test")
+    public JsonResult test(@RequestParam("name") String name,
+                           @RequestParam("pass") String pass) {
+        logger.info("name：{}", name);
+        logger.info("pass：{}", pass);
+        return new JsonResult();
+    }
+
+}
